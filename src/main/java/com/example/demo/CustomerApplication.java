970c8c73f4d6eafc7c78b71e8250526fa239c142
Yangzi Jiang <yangzi.jiang@payzer.com>
2021-06-01 16:21:05 -0400

    works again -> remove Spring actuator and downgrade parent to 2.3.10

--- src/main/java/com/example/demo/CustomerApplication.java
+++ src/main/java/com/example/demo/CustomerApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import lombok.extern.slf4j.Slf4j;
-
-
 import java.time.Instant;
 
 @SpringBootApplication
